e0a777faedbd9730b00ce83e818af71771498f49
ERM1106 <ermichalik1106@gmail.com>
2025-09-26 19:10:34 -0400

    Mep integration on webpage
    - Added the lines of code necessary to view the map through our web page.
    - All that is needed is the API Key and Map ID

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const UMBC_LAT = 39.256; //temp data
 const UMBC_LON = -76.711; //temp data
 
-function initmap() 
+function initMap() 
 {
     map = new google.maps.Map(document.getElementById('map'), {
         center: {lat: UMBC_LAT, lng: UMBC_LON},
