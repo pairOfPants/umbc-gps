5b46e38ea3d6cc889d0617f5c2456773e9f159be
Aidan Denham <adenham112@gmail.com>
2025-10-29 21:04:19 -0400

    Fixedf the things

--- testWebsite/main.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var inputValidate_1 = require("./inputValidate");
-// Example usage in your main app:
-var userInput = "Engineering Building";
-var words = (0, inputValidate_1.validateInput)(userInput);
-var suggestions = (0, inputValidate_1.suggestBuildingsFromInput)(words, _campusSuggestions);
